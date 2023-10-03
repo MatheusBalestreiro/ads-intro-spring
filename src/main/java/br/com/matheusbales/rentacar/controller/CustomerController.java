d0483c0d6db6aa86fab1a602671fedc8e1b58eae
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-10-03 08:52:12 -0300

    Documentando a API através do Swagger

--- src/main/java/br/com/matheusbales/rentacar/controller/CustomerController.java
+++ src/main/java/br/com/matheusbales/rentacar/controller/CustomerController.java
@@ -3,6 +3,11 @@ package br.com.matheusbales.rentacar.controller;
 
 import br.com.matheusbales.rentacar.dto.CustomerDTO;
 import br.com.matheusbales.rentacar.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
+
+@Tag(name = "Customers", description = "This endpoint manages Customers")
 public class CustomerController {
     @Autowired
     private CustomerService service;
@@ -21,7 +28,16 @@ public class CustomerController {
         return service.create(dto);
     }
 
+
     @GetMapping("/{id}")
+    @Operation(summary = "Persists a new Customer in database", tags = {"Customer"}, responses = {
+            @ApiResponse(description = "Success!", responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomerDTO.class))
+            }),
+            @ApiResponse(description = "Bad Request!", responseCode = "400", content = {@Content}),
+            @ApiResponse(description = "Unauthorized!", responseCode = "401", content = {@Content}),
+            @ApiResponse(description = "Internal Error!", responseCode = "500", content = {@Content})
+    })
     public CustomerDTO findById(@PathVariable("id") int id){
         return service.findById(id);
     }
