e7cdf452cfd08be0cb9c2b61ea53baff616d0976
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-10-10 10:02:55 -0300

    Trabalhando com migrations

---
+++ src/main/resources/db/migration/V1.0__Create_table_customers.sql
@@ -0,0 +1,7 @@
+create table if not exists customers(
+    id serial,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    city varchar(50) not null,
+    constraint pk_customers primary key (id)
+);
